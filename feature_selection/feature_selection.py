3046e8bfa3b30279fd06fa4b4673a2445e7b43be
VicenteAR <vicente.arjona@hotmail.com>
2022-02-01 18:08:19 +0100

    Corrected bugs.
    Signed-off-by: VicenteAR <vicente.arjona@hotmail.com>

--- feature_selection/feature_selection.py
+++ feature_selection/feature_selection.py
@@ -9,6 +9,7 @@ from pycaret.classification import (
     create_model,
     get_config,
     predict_model,
+    setup,
     tune_model,
 )
 from pycaret.utils import check_metric
@@ -120,7 +121,7 @@ class FeatureSelection(param.Parameterized):
     filter_metrics = param.Dict(_filter_metric)
     ## Model setup and model optimization parameters
     numerics = param.List(_numerics)
-    ignore_features = param.List(default=None, allow_None=True)
+    ignore_features = param.List(default=[], allow_None=True)
     setup_kwargs = param.Dict(_setup_kwargs)
     include = param.List(default=None, item_type=str, allow_None=True)
     exclude = param.List(["qda", "knn", "nb"], item_type=str)
@@ -146,7 +147,8 @@ class FeatureSelection(param.Parameterized):
         total_features = dataset.shape[1]
         self.param.target_features.bounds = (0, total_features)
         if "include" in kwargs:
-            self.param.number_models.bounds = (2, len(include))
+            self.param.number_models.default = len(kwargs["include"])
+            self.param.number_models.bounds = (0, len(kwargs["include"]))
         # Call super
         super(FeatureSelection, self).__init__(dataset=dataset, **kwargs)
         # Get the features of the dataframe
@@ -183,8 +185,8 @@ class FeatureSelection(param.Parameterized):
         if not self.include:
             args["exclude"] = self.exclude
         elif len(self.include) == 1:
-            training_function = create_model
-            args = {"estimator": self.include[0]}
+            training_function = lambda *rgs, **kwargs: [create_model(*rgs, **kwargs)]
+            args = {"estimator": self.include[0], "verbose": False}
         else:
             args["include"] = self.include
         return training_function, args
@@ -214,7 +216,7 @@ class FeatureSelection(param.Parameterized):
             c for c in self.ignore_features if c in self.feature_list
         ]
         # Initialize pycaret setup
-        setup(train_data=train_data, target=self.target, **self.setup_kwargs)
+        setup(data=train_data, target=self.target, **self.setup_kwargs)
         # Get train dataset and preprocessed dataframe
         self.x_train = get_config("X_train")
         if self.x_df.empty:  # TODO change x_df by dataset and add flag?
@@ -242,7 +244,11 @@ class FeatureSelection(param.Parameterized):
         self.tune_dict_models = {}
         for (model_str, py_model), optimize in product(self.dict_models.items(), self.opt_list):
             self.tune_dict_models[f"{model_str}_tune_{optimize}"] = tune_model(
-                py_model, optimize=optimize, verbose=False
+                py_model,
+                optimize=optimize,
+                verbose=False,
+                n_iter=30,
+                choose_better=True,
             )
 
     def get_metrics_df(self, test_predicted, model, dataframe):
@@ -298,7 +304,7 @@ class FeatureSelection(param.Parameterized):
             if (
                 model not in dict_models.keys()
             ):  # check no errors have been produced during operations
-                raise KeyError("The selected model is not listed in dict_models.keys()")
+                raise KeyError(f"The selected model: {model} is not listed in dict_models.keys()")
             df_conc = self.filter_best_features(key_model=model, models_dict=dict_models)
             self.features_df = pd.concat([self.features_df, df_conc])
 
@@ -325,7 +331,12 @@ class FeatureSelection(param.Parameterized):
             return x
 
         df = self.model_tuned_df.groupby("model").apply(drop)
-        return df.drop(columns="model").reset_index(level="model")
+        df = (
+            df.drop(columns="model").reset_index(level="model")
+            if isinstance(df.index, pd.MultiIndex)
+            else df
+        )
+        return df
 
     def tune_df(self):
         """Update self.features_df with the most relevant features used by tuned models."""
